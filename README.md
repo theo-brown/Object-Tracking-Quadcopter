bd1b98697803db5df861fc23d5e33a06e143ebe2
theo-b <theo@mcbrown.plus.com>
2015-09-22 20:58:37 +0000

    Full update

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Object tracking using OpenCV
 
 obj-track-colour uses HSV colour recognition to track objects.
+
+compile with g++ obj-track-colour-pi.cpp -o obj-track-colour-pi -I/usr/local/include/ -lraspicam -lraspicam_cv -lopencv_core -lopencv_highgui -lopencv_features2d -lopencv_imgproc
+
--- obj-track-colour-pi.cpp
+++ obj-track-colour-pi.cpp
@@ -18,6 +18,7 @@ int main()
     raspicam::RaspiCam_Cv capture;
     capture.set(CV_CAP_PROP_FORMAT, CV_8UC3);
     capture.open();
+    sleep(1);
     if (!capture.isOpened())
     {
         cout << "Failed to access webcam \n";
@@ -32,7 +33,7 @@ int main()
     // HSV adjust window
     namedWindow("Threshold", CV_WINDOW_AUTOSIZE);
     int threshHue=171, threshSat=125, threshVal=143;
-    int arealow=50,areahigh=10000,bloblow=1200,blobhigh=5000,blobd=50;
+    int arealow=45,areahigh=6500,bloblow=300,blobhigh=780,blobd=40;
     createTrackbar("Threshold Hue", "Threshold", &threshHue, 180);
     createTrackbar("Threshold Sat", "Threshold", &threshSat, 255);
     createTrackbar("Threshold Val", "Threshold", &threshVal, 255);
@@ -40,11 +41,11 @@ int main()
     // SET UP BLOB DETECTION
     SimpleBlobDetector::Params params;
     params.minDistBetweenBlobs = blobd;
-    params.minThreshold = bloblow;
-    params.maxThreshold = blobhigh;
+    //params.minThreshold = bloblow;
+    //params.maxThreshold = blobhigh;
     params.filterByArea = true;
-    params.minArea = arealow;
-    params.maxArea = areahigh;
+    //params.minArea = arealow;
+    //params.maxArea = areahigh;
     params.filterByInertia = false;
     params.filterByConvexity = false;
     params.filterByColor = false;
@@ -66,7 +67,9 @@ int main()
             cout << "Error retrieving frame" << endl;
             return -1;
         }
-        resize(frame,frame,Size(480,320),0,0,CV_INTER_AREA);
+        resize(frame,frame,Size(320,240),0,0,CV_INTER_AREA);
+        //resize(frame,frame,Size(240,160),0,0,CV_INTER_AREA);
+	flip(frame, frame, 0);
         imshow("Preview", frame);
 
         // Convert to HSV
@@ -110,7 +113,9 @@ int main()
 	capture.grab();
         capture.retrieve (frame);
         if(frame.empty()) break;
-        resize(frame,frame,Size(480,320),0,0,CV_INTER_AREA);
+        resize(frame,frame,Size(320,240),0,0,CV_INTER_AREA);
+        //resize(frame,frame,Size(240,160),0,0,CV_INTER_AREA);
+	flip(frame,frame,0);
 
         // Convert to HSV
         frame_hsv.create(frame.size(), frame.type());
@@ -121,9 +126,10 @@ int main()
 
         // Detect blobs
         blobdetect->detect(frame_thresh, keypoints);
-        drawKeypoints(frame, keypoints, frame_processed, Scalar(0,0,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        //drawKeypoints(frame, keypoints, frame_processed, Scalar(0,0,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
-        // Get coordinates
+	/*
+        // Get coordinates DEPRECATED
         if(keypoints.size() > 0 && keypoints.size() < 3) // Some large objects split into 2 keypoints, include these as 1
         {
             Point2f centre = Point(frame.cols/2,frame.rows/2);
@@ -134,9 +140,39 @@ int main()
 
             cout << "Diff X:" << kpt_err.x << " Y: " << kpt_err.y << endl;
         }
+	*/
+
+	// Find mean of first 5 keypoints:
+        float tot_x=0, tot_y=0, mean_x, mean_y, tot_size=0, mean_size;
+	int no_kpts=0;
+        for(no_kpts; no_kpts<keypoints.size() && no_kpts<=5; no_kpts++)
+	{
+	    tot_x += keypoints[no_kpts].pt.x;
+	    tot_y += keypoints[no_kpts].pt.y;
+	    tot_size += keypoints[no_kpts].size;
+	}
+        mean_x = tot_x / no_kpts;
+	mean_y = tot_y / no_kpts;
+	mean_size = tot_size / no_kpts
+
+
+	// Return keypoint distance from centre
+	Point2f centre = Point(frame.cols/2, frame.rows/2);
+        Point2f mean_pt = Point(mean_x, mean_y);
+	Point2f pt_err = centre - mean_pt;
+	if(pt_err.x == 160 && pt_err.y == 120);
+	{
+	    pt_err.x = 0;
+	    pt_err.y = 0;
+	}
+
+        cout << "Diff X:" << pt_err.x << " Y: " << pt_err.y << endl;
+
+        // Draw on mean point
+        circle(frame, mean_pt, mean_size, Scalar(0,0,0));
 
         // Display frame
-        imshow("Preview", frame_processed);
+        imshow("Preview", frame);
 
         // Exit if q pressed
         char c = waitKey(20);
@@ -146,7 +182,9 @@ int main()
             break;
         }
     }
+    cout << "Releasing camera... ";
     capture.release();
+    cout << "Done." << endl;
     return 0;
 }
 
